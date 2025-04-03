fb645da3e7c2762022c445d8a74f01d0376b006d
Code0987 <macqueen0987@gmail.com>
2025-04-04 00:43:37 +0900

    변수를 모두 env 파일로 이동함에 따라 var.py 숨길 필요 없음

---
+++ backend/var.py
@@ -0,0 +1,13 @@
+import os
+
+API_ENDPOINT = os.environ["DISCORD_API_URL"]
+CLIENT_ID = os.environ["DISCORD_CLIENT_ID"]
+CLIENT_SECRET = os.environ["DISCORD_SECRET"]
+REDIRECT_URI = os.environ["REDIRECT_URI"]
+
+dbuser = os.environ["MYSQL_USER"]
+dbpassword = os.environ["MYSQL_PASSWORD"]
+dbhost = "notipy-database"
+dbport = os.environ["MYSQL_TCP_PORT"]
+dbname = os.environ["MYSQL_DATABASE"]
+
---
+++ discordbot/commons/var.py
@@ -0,0 +1,12 @@
+import os
+
+token = os.environ["DISCORD_TOKEN"]
+
+devserver = os.environ['DISCORD_DEVSERVER']  # Replace with actual server ID
+
+developers = os.environ['DISCORD_DEVELOPERS']  # Replace with actual developer IDs
+
+
+# logfiles, they will be located at discordbot/logs
+debugfile = os.environ['DISCORD_DEBUG_FILE']  # File to log debug information
+errfile = os.environ['DISCORD_ERROR_LOG']  # File to log error information
