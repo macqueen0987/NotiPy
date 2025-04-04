42a754417c02f34654485f5c1630d95455f9d95d
Code0987 <macqueen0987@gmail.com>
2025-04-04 23:04:55 +0900

    추후 깃허브 토큰이 필요할지도 모르니 추가

--- backend/var.py
+++ backend/var.py
@@ -1,6 +1,9 @@
 import os
 
-API_ENDPOINT = os.environ["DISCORD_API_URL"]
+GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+GITHUB_API_ENDPOINT = os.environ["GITHUB_API_URL"]
+
+DISCORD_API_ENDPOINT = os.environ["DISCORD_API_URL"]
 CLIENT_ID = os.environ["DISCORD_CLIENT_ID"]
 CLIENT_SECRET = os.environ["DISCORD_SECRET"]
 REDIRECT_URI = os.environ["REDIRECT_URI"]
