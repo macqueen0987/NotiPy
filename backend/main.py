ab5c46ebe9263a24f73f85aef3725e57f31f7398
Code0987 <macqueen0987@gmail.com>
2025-04-04 19:05:11 +0900

    프론트가 많이 없으니 그냥 간단하게 프론트까지 담당하도록변경, 자동으로 모든 router 탑재

--- backend/main.py
+++ backend/main.py
@@ -1,47 +1,39 @@
 import aiohttp
-from fastapi import FastAPI, Request, BackgroundTasks
+from fastapi import FastAPI, Request, BackgroundTasks, Depends
 from fastapi.responses import JSONResponse
+from sqlalchemy import select
+import os
+import uvicorn
+import pkgutil
+
 from var import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, API_ENDPOINT
 from datetime import datetime
 import db.models as models
 from common import get_db
 
 app = FastAPI()
+api = FastAPI()
 auth = aiohttp.BasicAuth(CLIENT_ID, CLIENT_SECRET)
 
 @app.get("/")
-async def root(request: Request, bgtask: BackgroundTasks, code: str = None):
-    print(request.headers)
-    bgtask.add_task(exchange_code, code)
-    return JSONResponse({"hello": "world"})
+async def root(request: Request):
+    return JSONResponse(content={"message": "Hello World!"})
 
-async def exchange_code(code: str):
-    data = {
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': REDIRECT_URI,
-    }
-    headers = {"content-type": "application/x-www-form-urlencoded"}
-    async with aiohttp.ClientSession() as session:
-        async with session.post(f'{API_ENDPOINT}/oauth2/token', data=data, headers=headers, auth=auth) as response:
-            if response.status != 200:
-                print(f"Error: {response.status}")
-                return
-            data = await response.json()
-            access_token = data['access_token']
-            refresh_token = data['refresh_token']
-            print(await response.json())
-    await get_connected(access_token)
 
-async def get_connected(access_token:str):
-    headers = {"content-type": "application/x-www-form-urlencoded", "Authorization": f"Bearer {access_token}"}
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f'{API_ENDPOINT}/users/@me/connections', headers=headers) as response:
-            print(response.status)
-            print(response.text)
-            print(await response.json())
+@app.get("/test")
+async def test(request: Request, conn=Depends(get_db)):
+    query = select(models.User)
+    result = await conn.execute(query)
+    print(result)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    routers = []
+    for loader, module_name, is_pkg in pkgutil.iter_modules(["./routers"], "routers."):
+        routers.append(module_name)
+    for router in routers:
+        module = __import__(router, fromlist=["router"])
+        api.include_router(module.router)
+    app.mount("/api", api)
+    port = int(os.environ.get("BACKEND_PORT", 9091))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
