23f8e0139f4464e4e4d2ea0c5cba927c1d3b7de0
Code0987 <macqueen0987@gmail.com>
2025-04-08 18:14:40 +0900

    변수를 몇가지 더 추가

--- discordbot/commons/var.py
+++ discordbot/commons/var.py
@@ -10,5 +10,13 @@ developers = [int(dev) for dev in developers.split(",")]
 # logfiles, they will be located at discordbot/logs
 debugfile = os.environ['DISCORD_DEBUG_FILE']  # File to log debug information
 errfile = os.environ['DISCORD_ERROR_LOG']  # File to log error information
+apiport = os.environ['BACKEND_PORT']
 
-app_root = "/discord"
+app_root = os.environ['DISCORD_APP_ROOT']
+backend_url = "http://notipy-backend"
+backend_api_root = os.environ['BACKEND_APP_ROOT']
+
+
+api_root = f"{backend_url}:{apiport}{backend_api_root}"  # Backend API root URL
+
+githuburl = "https://github.com"
