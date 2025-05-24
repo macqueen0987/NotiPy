ce44548d1dd3c199d65c9d756e781a746608cdee
Code0987 <macqueen0987@gmail.com>
2025-05-24 11:17:18 +0900

    API endpoint RESTful 하도록 변경

--- discordbot/extensions/Misc.py
+++ discordbot/extensions/Misc.py
@@ -13,6 +13,15 @@ class Misc(Extension):
 
     def __init__(self, bot):
         self.bot = bot
+        asyncio.create_task(self.async_init())
+
+    async def async_init(self):
+        """
+        on ready, update all servers, and remove servers that are not in the bot's guilds
+        """
+        guilds = self.bot.guilds
+        guilids = [int(guild.id) for guild in guilds]
+        await apirequest(f"/discord/clean", json=guilids, method="POST")
 
     @listen(GuildJoin)
     async def on_guild_join(self, guild: GuildJoin):
@@ -20,32 +29,22 @@ class Misc(Extension):
             return
         guildname = guild.guild.name
         guildid = guild.guild_id
-        await apirequest(f"/discord/updateserver?serverid={guildid}")
-        dmchannel = await self.bot.owner.fetch_dm()
+        await apirequest(f"/discord/{guildid}", method="POST")
+        dmchannel = await self.bot.owner.fetch_dm(force=True)
         await dmchannel.send(f"bot invited: {guildname}")
 
     @listen(GuildLeft)
     async def on_guild_left(self, guild: GuildLeft):
         guildname = guild.guild.name
         guildid = guild.guild_id
-        await apirequest(f"/discord/removeserver?serverid={guildid}")
-        dmchannel = await self.bot.owner.fetch_dm()
+        await apirequest(f"/discord/{guildid}", method="DELETE")
+        dmchannel = await self.bot.owner.fetch_dm(force=True)
         await dmchannel.send(f"bot removed: {guildname}")
-
-    @listen(Ready)
-    async def on_ready(self):
-        """
-        on ready, update all servers, and remove servers that are not in the bot's guilds
-        """
-        guilds = self.bot.guilds
-        for guild in guilds:
-            guildid = guild.id
-            await apirequest(f"/discord/updateserver?serverid={guildid}")
-        await apirequest("/discord/removeoldservers")
 
     @modroleGroup.subcommand(
         sub_cmd_name=getname("set"), sub_cmd_description=getdesc("set_modrole")
     )
+    @check(is_moderator)
     @localize()
     @roleOption()
     async def set_modrole(self, ctx: SlashContext, _, role):
@@ -54,16 +53,15 @@ class Misc(Extension):
         """
         guildid = int(ctx.guild_id)
         roleid = int(role.id)
-        status, response = await apirequest(
-            f"/discord/setmodrole?serverid={guildid}&roleid={roleid}"
-        )
+        status, response = await apirequest(endpoint=f"/discord/{guildid}/modrole", method="PUT", json=roleid)
         if status != 200:
-            raise ValueError("Error in /discord/setmodrole")
+            raise ValueError(f"Error in /discord/{guildid}/modrole")
         modcache[guildid] = roleid
         await ctx.send(_("set_modrole_success"), ephemeral=True)
 
     @settingsBase.subcommand(sub_cmd_name=getname("view"),
                              sub_cmd_description=getdesc("view_settings"))
+    @check(is_moderator)
     @localize()
     async def view_settings(self, ctx: SlashContext, _):
         """
@@ -71,9 +69,9 @@ class Misc(Extension):
         """
         guild = ctx.guild
         guildid = int(ctx.guild_id)
-        status, response = await apirequest(f"/discord/get?serverid={guildid}")
+        status, response = await apirequest(f"/discord/{guildid}", params={"eager_load": 1})
         if status != 200:
-            raise ValueError("Error in /discord/getsettings")
+            raise ValueError(f"Error in /discord/{guildid}")
         if response["server"] is None:
             await ctx.send(_("no_settings"), ephemeral=True)
             return
@@ -89,6 +87,47 @@ class Misc(Extension):
             else:
                 mod = f"<@&{mod.id}>"
         embed.add_field(name=_("modrole"), value=mod, inline=True)
+        webhookchannel = server["webhook_channel_id"]
+        if webhookchannel is None:
+            webhookchannel = _("not_set")
+        else:
+            webhookchannel = await guild.fetch_channel(webhookchannel)
+            if webhookchannel is None:
+                webhookchannel = _("none_channel")
+            else:
+                webhookchannel = webhookchannel.mention
+        embed.add_field(name=_("webhook_channel"), value=webhookchannel, inline=True)
+        notiontoken = server["notion_token"]
+        if notiontoken is None:
+            notiontoken = _("not_set")
+        else:
+            notiontoken = _("setted")
+        embed.add_field(name=_("notiontoken"), value=notiontoken, inline=True)
+        notion_tags = server["notion_tags"]
+        tags = []
+        for tag in notion_tags:
+            tags.append(tag["tag"])
+        if not tags:
+            tags = [_("not_set")]
+        else:
+            tags = ", ".join(tags)
+        embed.add_field(name=_("notiontag"), value=tags, inline=True)
+        notion_databases = server["notion_databases"]
+        databases = []
+        for database in notion_databases:
+            channel = database['channel_id']
+            channel = await guild.fetch_channel(channel)
+            if channel is None:
+                channel = _("none_channel")
+            else:
+                channel = channel.mention
+            msg = f"{database['database_name']} → {channel}"
+            databases.append(msg)
+        if not databases:
+            databases = [_("not_set")]
+        else:
+            databases = "\n".join(databases)
+        embed.add_field(name=_("notion_databases"), value=databases, inline=False)
         await ctx.send(embeds=embed, ephemeral=True)
 
 
