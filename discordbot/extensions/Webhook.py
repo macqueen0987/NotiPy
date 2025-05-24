03b27988a7be1a464ad0dab1cff94dfb9fd72a9b
Code0987 <macqueen0987@gmail.com>
2025-05-24 11:16:24 +0900

    웹훅 종류에 따른 색 지정

--- discordbot/extensions/Webhook.py
+++ discordbot/extensions/Webhook.py
@@ -37,13 +37,30 @@ class Webhook(Extension):
         """
         channelid = int(ctx.channel.id)
         guildid = int(ctx.guild_id)
-        params = {"serverid": guildid, "channelid": channelid}
-        status, response = await apirequest("/discord/setwebhookchannel", params=params)
+        status, response = await apirequest(f"/discord/{guildid}/webhookchannel", method="PUT", json=channelid)
         if status != 200:
             raise ValueError("Error in /discord/setwebhookchannel")
         await ctx.send(_("set_webhook_channel_success"), ephemeral=True)
 
-
+colors = {
+    "page.content_updated": 0x7f8c8d,
+    "page.created": 0xe74c3c,
+    "page.deleted": 0x2ecc71,
+    "page.locked": 0x9b59b6,
+    "page.moved": 0x34495e,
+    "page.properties_updated": 0xf1c40f,
+    "page.undeleted": 0x3498db,
+    "page.unlocked": 0x1abc9c,
+    "database.content_updated": 0xe67e22,
+    "database.created": 0xfd79a8,
+    "database.deleted": 0x2d3436,
+    "database.moved": 0x8e44ad,
+    "database.schema_updated": 0x16a085,
+    "database.undeleted": 0xf39c12,
+    "comment.created": 0x2980b9,
+    "comment.deleted": 0xc0392b,
+    "comment.updated": 0x27ae60
+}
 async def notionWebhook(bot: Client, params: dict) -> None:
     """
     Notion webhook handler.
@@ -58,13 +75,12 @@ async def notionWebhook(bot: Client, params: dict) -> None:
         return
     locale = channel.guild.preferred_locale
     type_ = getlocale(params["type"], locale)
-    embed = Embed(title=type_, color=createRandomColor(),
+    embed = Embed(title=type_, color=colors[params["type"]],
                   description=params["workspace_name"])
     if params["pageurl"] is not None:
         embed.url = params["pageurl"]
     if params["pagetitle"] is not None:
-        embed.description = params["workspace_name"] + \
-            " - " + params["pagetitle"]
+        embed.description = params["workspace_name"] + " - " + params["pagetitle"]
     embed.set_author(name=str(params["author"]), icon_url=params["avatar_url"])
     if channel is None:
         return
